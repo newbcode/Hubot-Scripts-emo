6c4e1c7f0cb5dde96d1c354f5a22d4036fd9213d
newbcode <newbcode@newb-dtop>
2013-06-18 23:18:31 +0900

    at home commit

--- lib/Hubot/Scripts/emo.pm
+++ lib/Hubot/Scripts/emo.pm
@@ -3,5 +3,15 @@ package Hubot::Scripts::emo
 use strict;
 use warnings;
 
+sub load {
+    my ($class, $robot ) = @_;
+    qr/^emo (.+)/i;
+        \&emo_process,
+};
+
+sub emo_process {
+
+        
+}
 
 
