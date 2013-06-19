ff619f3f38d223debb86f4180c8efafcfa2bfd70
newbcode <newbcode@newb-dtop>
2013-06-20 00:55:29 +0900

    Add emoticons and Storable

--- lib/Hubot/Scripts/emo.pm
+++ lib/Hubot/Scripts/emo.pm
@@ -1,17 +1,63 @@
-package Hubot::Scripts::emo
+package Hubot::Scripts::emo;
 
+use utf8;
 use strict;
 use warnings;
+use Storable;
 
 sub load {
-    my ($class, $robot ) = @_;
-    qr/^emo (.+)/i;
+    my ( $class, $robot ) = @_;
+ 
+    $robot->hear(
+        qr/^emo (.+)/i,    
         \&emo_process,
-};
+    );
+}
 
 sub emo_process {
+    my $msg = shift;
 
-        
+    my $emot1 = '
+                ^^              @@@@@@@@@
+       ^^       ^^            @@@@@@@@@@@@@@@
+                            @@@@@@@@@@@@@@@@@@              ^^
+                           @@@@@@@@@@@@@@@@@@@@
+ ~~~~ ~~ ~~~~~ ~~~~~~~~ ~~ &&&&&&&&&&&&&&&&&&&& ~~~~~~~ ~~~~~~~~~~~ ~~~
+ ~         ~~   ~  ~       ~~~~~~~~~~~~~~~~~~~~ ~       ~~     ~~ ~
+   ~      ~~      ~~ ~~ ~~  ~~~~~~~~~~~~~ ~~~~  ~     ~~~    ~ ~~~  ~ ~~
+   ~  ~~     ~         ~      ~~~~~~  ~~ ~~~       ~~ ~ ~~  ~~ ~
+ ~  ~       ~ ~      ~           ~~ ~~~~~~  ~      ~~  ~             ~~
+       ~             ~        ~      ~      ~~   ~             ~ 
+    
+    ';
+    store \$emot1, 'emoticonsA';
+    my $emoticon = retrieve('emoticonsA');
+
+    $msg->send("\n", split /\n/, $$emoticon);
 }
 
 
+1;
+
+=pod
+
+=head1 Name 
+
+    Hubot::Scripts::emo
+ 
+=head1 SYNOPSIS
+
+    emo <emoticons> - Show Emoticons
+ 
+=head1 AUTHOR
+
+    YunChang Kang <codenewb@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Yunchang Kang.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself
+ 
+=cut
